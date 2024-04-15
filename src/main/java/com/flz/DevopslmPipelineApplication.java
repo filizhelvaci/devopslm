e43bda33853842bf81d7e5537b34f6dfea9278d1
filizhelvaci <filizhelvaci82@gmail.com>
2024-04-15 09:39:59 +0300

    Devopslm Pipeline
    Project created.

---
+++ src/main/java/com/flz/DevopslmPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.flz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopslmPipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopslmPipelineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flz/DevopslmPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopslmPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
